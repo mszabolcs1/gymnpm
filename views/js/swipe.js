0742a1ccf1fd02ce394ed508520c7bef3aaeaca6
Szabolcs <matyi.szabolcs@gmail.com>
2022-07-27 20:01:04 +0200

    timer, gradient

--- views/js/swipe.js
+++ views/js/swipe.js
@@ -13,8 +13,6 @@ window.onload = (event) => {
 
 
 
-
-
 pageCount = 0
 setCount = 1
 swiping = false
@@ -32,7 +30,7 @@ async function switchPage(mode) {
         pageCount += 1
         running = true
         document.getElementById('startbtn').innerHTML = 'END'
-        newPage.style.background = 'red'
+        newPage.style.background = "rgb(144,238,144)"
         newPage.innerHTML = '<div class="excercise">EXCERCISE 1</div><div class="timer">00:20</div><div class="set">SET 1</div>'
         document.getElementById('container').appendChild(newPage)
         updateExcerciseCounter(pageCount)
@@ -45,27 +43,29 @@ async function switchPage(mode) {
         document.getElementById('container').appendChild(newPage)
     }
     
+    endOfSet = new Date().getTime() + setDuration + 1000
     
     //document.getElementsByClassName('page')[0].setAttribute('style', 'animation: fade 2s cubic-bezier(0.76, 0, 0.24, 1) 0s 1 normal forwards;')
     
-
+    //add click event listener to new page
     document.getElementsByClassName('page')[1].addEventListener("click", updateSetCounter);
     
-
-    fadeOldPage()
+    //wtf
+    if (pageCount > 1) {
+        //fadeOldPage()
+    }
 
     //delete old page
     await sleep(500)
     document.getElementsByClassName('page')[0].remove()
-
-
+    //reenable swiping
     swiping = false
 
 }
+
 async function fadeOldPage() {
     for (let i = 0; i < 40; i++) {
-        console.log('fade')
-        document.getElementsByClassName('page')[0].style.filter = "grayscale(" + i + "%)"
+        document.getElementsByClassName('page')[0].style.filter = "brightness(" + (100-i/1.5) + "%)"
         await sleep(5)
     }
 }
@@ -86,4 +86,5 @@ function updateSetCounter() {
     setCount += 1
     console.log(setCount)
     document.getElementsByClassName('set')[0].innerHTML = 'SET ' + setCount
+    endOfSet = new Date().getTime() + setDuration + 800
 }
--- views/js/timer.js
+++ views/js/timer.js
@@ -6,22 +6,83 @@ window.addEventListener('load', (event) => {
 
 running = false
 function setOrStart() {
-    if (running == false) {
-        running = true
-        document.getElementById('startbtn').innerHTML = 'END'
-        switchPage(false)
-    }
-    else if (running == true) {
-        running = false
-        document.getElementById('startbtn').innerHTML = 'START'
-        switchPage(true)
+    if (swiping == false) {
+        if (running == false) {
+            running = true
+            document.getElementById('startbtn').innerHTML = 'END'
+            switchPage(false)
+        }
+        else if (running == true) {
+            running = false
+            document.getElementById('startbtn').innerHTML = 'START'
+            switchPage(true)
+        }
     }
 }
 
 
 
 
+///////////////////////////////////////////////
+///////////////////////////////////////////////
+
+setDuration = 12000
+warningTime = setDuration/4
+refreshInterval = 100
+setInterval(function() {
+    if (running == true) {
+
+        // SET TIME
+        currentTime = new Date().getTime()
+        setTime = endOfSet - currentTime
+
+        setMinutes = Math.floor((setTime % (1000 * 60 * 60)) / (1000 * 60)), 2
+        setSeconds = Math.abs(Math.floor((setTime % (1000 * 60)) / 1000)), 2
+
+        if (document.getElementsByClassName('timer').length == 1) {
+            //continue countdown if new page not present
+            document.getElementsByClassName('timer')[0].innerHTML = pad(setMinutes) + ":" + pad(setSeconds)
+        }
+        else {
+            //
+            document.getElementsByClassName('timer')[1].innerHTML = pad(Math.floor(((setDuration+500) % (1000 * 60 * 60)) / (1000 * 60)), 2) + ":" + pad(Math.abs(Math.floor(((setDuration+500) % (1000 * 60)) / 1000)), 2)
+            
+        }
+        
+
+        
+        // BACKGROUND-COLOR
+        if (document.getElementsByClassName('timer').length == 1) {
+            if (setTime < 0) {
+                //console.log(setTime)
+            }
+            else if (setTime < warningTime) {
+                document.getElementsByClassName('page')[0].style = "background-color: rgb("+
+                    Math.round((1-(setTime/warningTime)) * (red[0]-yellow[0])+yellow[0]) + ", " +
+                    Math.round((1-(setTime/warningTime)) * (red[1]-yellow[1])+yellow[1]) + ", " +
+                    Math.round((1-(setTime/warningTime)) * (red[2]-yellow[2])+yellow[2]) + 
+                ");"
+            }
+            else {
+                document.getElementsByClassName('page')[0].style = "background-color: rgb("+
+                    Math.round((1-((setTime-warningTime)/(setDuration-warningTime))) * (yellow[0]-green[0])+green[0]) + ", " +
+                    Math.round((1-((setTime-warningTime)/(setDuration-warningTime))) * (yellow[1]-green[1])+green[1]) + ", " +
+                    Math.round((1-((setTime-warningTime)/(setDuration-warningTime))) * (yellow[2]-green[2])+green[2]) + 
+                ");"
+            }
+        }
+
+        
+    }
+}, refreshInterval)
+
 
 green = [144, 238, 144]
 yellow = [255, 210, 76]
 red = [255, 73, 70]
+
+
+function pad(num) {
+    var s = "000000000" + num;
+    return s.substr(s.length - 2);
+}
